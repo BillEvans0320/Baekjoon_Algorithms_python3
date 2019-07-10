9ff5b0e252c5137bfe4716387f7aa5d42ec80e33
BillEvans0320 <gmh0320@snu.ac.kr>
2019-07-10 13:30:04 +0900

    print 'A + B', using 'while'

--- 10951.py
+++ 10951.py
@@ -2,5 +2,8 @@
 import sys
 
 while True:
-    A, B = list(map(int, sys.stdin.readline().split()))
-    print(A+B)
+    try:
+        A, B = list(map(int, sys.stdin.readline().split()))
+        print(A+B)
+    except:
+        break
